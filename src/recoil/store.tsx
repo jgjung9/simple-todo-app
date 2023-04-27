c4af41d24b4882d5b6f44cb53b650fbec841b6e3
Gio <wnsgyo9@gmail.com>
2023-04-27 23:49:14 +0900

    Add todo list state and filtered list state in recoil

--- src/recoil/store.tsx
+++ src/recoil/store.tsx
@@ -10,3 +10,33 @@ export const filterState = atom({
   key: 'todoFilter',
   default: 'All',
 });
+
+export interface Todo {
+  id: string;
+  text: string;
+  isComplete: boolean;
+}
+
+export const todoList = atom<Todo[]>({
+  key: 'todoList',
+  default: [],
+});
+
+export const filteredTodoList = selector<Todo[]>({
+  key: 'filteredTodoList',
+  get: ({ get }) => {
+    const todos = get(todoList);
+    const filter = get(filterState);
+
+    switch (filter) {
+      case 'All':
+        return todos;
+      case 'Active':
+        return todos.filter((todo) => !todo.isComplete);
+      case 'Completed':
+        return todos.filter((todo) => todo.isComplete);
+      default:
+        throw new Error(`${filter} is invaild filter`);
+    }
+  },
+});
